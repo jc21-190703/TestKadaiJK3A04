f1ae3cceba7ba269cfa4158c8c72ca4a2a20fdfe
愛梨 太田 <190703@jc-21.jp>
2022-02-15 16:58:16 +0900

    jetcoaster：待ち行列が｛｝のとき、100人乗りを100回動かしたとき 成功

--- JetCoaster.cs
+++ JetCoaster.cs
@@ -11,16 +11,16 @@ namespace WindowsFormsApp2
         //k:定員、g:配列、ｒ:回す回数
 
         static int i = 0;
-        
+
 
         public static int ride(int k, int[] g, int r)//メイン
         {
             i = 0;
             int saisyukekka = 0;
-           
+
             for (int j = 1; j <= r; j++)//回す回数
             {
-                saisyukekka += ride2(k,g);
+                saisyukekka += ride2(k, g);
             }
             return saisyukekka;
 
@@ -29,33 +29,50 @@ namespace WindowsFormsApp2
 
 
         public static int ride2(int k, int[] g)//定員、配列
-        { 
+        {
             int kekka = 0;
 
             Boolean flg = true;
             while (flg)
             {
                 Console.Write(i);
-                if (kekka + g[i] > k)
+                if (g.Length == 0)
                 {
+                    kekka = 0;
                     flg = false;
-                   
                 }
                 else
                 {
-                   kekka += g[i];
-                    if (g.Length == 1)
+                    if (kekka + g[i] > k)
                     {
                         flg = false;
-                    }
-                   i = (i + 1) % g.Length;
-                } 
-                
-            } 
 
+                    }
+                    else
+                    {
+                        if (g.Length == 1)
+                        {
+                            kekka += g[i];
+                            flg = false;
+                        }
+                        else
+                        {
+                            kekka += g[i];
+                            i = (i + 1) % g.Length;
+                        }
+                    }
+
+                }
+            }
             return kekka;
-           
         }
 
+
+
     }
+
 }
+
+
+
+
